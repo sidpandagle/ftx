a9d515025fd1ab90a538a4381756e3d7c47aa0c9
Siddhant Pandagle <siddhant.pandagle1998@gmail.com>
2024-07-12 12:22:50 +0530

    Google Analytics Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import Footer from "@/shared/Footer";
 import Header from "@/shared/Header";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header></Header>
         {children}
         <Footer></Footer>
+        <GoogleAnalytics gaId="G-T2MWL6PH5Y" />
       </body>
     </html>
   );
