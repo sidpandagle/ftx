58d4b33178e8ab2531132c1e797aa12309ea2912
fudge-fantastic <siddhant.pandagle1998@gmail.com>
2024-07-15 22:15:37 +0530

    Gradient Keyword updated

--- src/app/gradient/page.tsx
+++ src/app/gradient/page.tsx
@@ -4,8 +4,8 @@ import Gradient from "./Gradient";
 
 export const metadata: Metadata = {
   title: "Gradients",
-  description: "Level Up Your Design: Unleash the Power of Gradients",
-  keywords: "online converter, free converter, file converter, document converter, image converter, audio converter, video converter, unit converter, data converter, online tool, free tool, easy to use, fast conversion, batch conversion",
+  description: "Ditch the color wheel struggle! We've got thousands of awesome color gradients, always fresh and updated daily. Snag the perfect one for your next design and bookmark your faves!",
+  keywords: "color gradients, design gradients, trendy gradients, gradient library, gradient tool, color palette generator, web design inspiration, graphic design tools, UI/UX design, color inspiration, download free color gradients, create custom color gradients, how to use color gradients in design, best color gradients for websites, find the perfect color gradient for your project, design trends, color theory, visual communication, user experience, user interface",
 };
 
 export default function Dashboard() {
